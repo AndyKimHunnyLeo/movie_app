980c3fc948e2a61b104d109cde5df82ad410d03c
Andy Kim <hyungSeok.kim@outlook.com>
2020-03-10 16:27:44 +1300

    test

---
+++ build/precache-manifest.363d5ea30c4b67f2f9610c3bdfeebb16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b84a01f3df24c1df78dc985ff501b7c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c98e8e647d2cfbd475b2",
+    "url": "/movie_app/static/css/main.a7548cb1.chunk.css"
+  },
+  {
+    "revision": "4bb2325b79a3a00b46bd",
+    "url": "/movie_app/static/js/2.c147d183.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.c147d183.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c98e8e647d2cfbd475b2",
+    "url": "/movie_app/static/js/main.81a8a158.chunk.js"
+  },
+  {
+    "revision": "3dd3f4ef526798ea7005",
+    "url": "/movie_app/static/js/runtime-main.ec52c1d1.js"
+  }
+]);
