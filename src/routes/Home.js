06a3ad3be7eb053bad4249d556ea28ef87c00541
Andy Kim <hyungSeok.kim@outlook.com>
2020-03-10 16:19:33 +1300

    init commit

---
+++ src/routes/Home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Moive';
+import "./Home.css";
+
+class Home extends React.Component {
+
+    state = {
+        isloading: true,
+        movie: []
+    };
+    getMovies = async () => {
+        const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+        this.setState({ movies, isloading: false });
+    }
+
+    componentDidMount() {
+        this.getMovies();
+
+    }
+
+    render() {
+        const { isloading, movies } = this.state;
+        return <section className="container">
+            {isloading ?
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
+                </div>
+                : (
+                    <div className="movies">
+                        {movies.map(movie => (
+                            < Movie key={movie.id}
+                                id={movie.id}
+                                title={movie.title}
+                                year={movie.year}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                        ))}
+                    </div>
+                )}
+        </section>;
+    }
+}
+
+export default Home;
